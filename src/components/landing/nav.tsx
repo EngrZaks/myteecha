b5c382e606d618a7a404c57e11259f8bf6075dbf
DevZaks <abzakariyya@gmail.com>
2021-09-18 13:26:14 +0100

    implemented auto-scroll functionality

--- src/components/landing/nav.tsx
+++ src/components/landing/nav.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import "antd/dist/antd.css";
-
+// import "./style.scss";
 import { MenuOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
+import useScrollListener from "../../utils/useScroll";
 const { SubMenu } = Menu;
 interface singinSignupParams {
   logo: any;
@@ -15,9 +16,19 @@ export const MobileNav: React.FC<singinSignupParams> = ({
   showSignup,
   showSignin,
 }) => {
-  // const windowWidth = useWindowSize().width;
+  const [navClassList, setNavClassList] = React.useState([]);
+  const scroll = useScrollListener();
+  // update classList of nav on scroll
+  React.useEffect(() => {
+    const _classList: any = [];
+
+    if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
+      _classList.push("nav-bar--hidden");
+
+    setNavClassList(_classList);
+  }, [scroll.y, scroll.lastY]);
   return (
-    <nav>
+    <nav className={navClassList.join()}>
       <Menu
         className="menu"
         style={{ width: "100%", padding: 5, zIndex: 1 }}
@@ -62,8 +73,20 @@ export const DesktopNav: React.FC<singinSignupParams> = ({
   showSignin,
   showSignup,
 }) => {
+  const [navClassList, setNavClassList] = React.useState([]);
+  const scroll = useScrollListener();
+  // update classList of nav on scroll
+  React.useEffect(() => {
+    const _classList: any = [];
+
+    if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
+      _classList.push("nav-bar--hidden");
+
+    setNavClassList(_classList);
+  }, [scroll.y, scroll.lastY]);
   return (
-    <nav className="desktop-nav">
+    // <nav className="desktop-nav">
+    <nav className={navClassList.join() + " desktop-nav"}>
       <div className="logo">
         <img src={logo} alt="myteecha logo" />
       </div>
